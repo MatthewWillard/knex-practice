232ffc946b94b5cdbfd7becf59b93f2d36d90328
Matthew Willard <MWillard85@gmail.com>
2019-08-01 16:49:32 -0600

    finished assignment

---
+++ src/drills.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+})
+//DRILL 1
+function searchByName(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log('SEARCH TERM', { searchTerm })
+      console.log(result)
+    })
+}
+
+searchByName()
+//DRILL 2
+function paginateItems(page) {
+  const limit = 6
+  const offset = limit * (page - 1)
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(limit)
+    .offset(offset)
+    .then(result => {
+      console.log('PAGINATE ITEMS', { page })
+      console.log(result)
+    })
+}
+
+paginateItems(4)
+//DRILL 3
+function addedDaysAgo(daysAgo) {
+  knexInstance
+    .select('id', 'name', 'price', 'date_added', 'checked', 'category')
+    .from('shopping_list')
+    .where(
+      'date_added',
+      '>',
+      knexInstance.raw(`now() - '?? days':: INTERVAL`, daysAgo)
+    )
+    .then(results => {
+      console.log('PRODUCTS ADDED DAYS AGO')
+      console.log(results)
+    })
+}
+
+addedDaysAgo(2)
+//DRILL 4
+function costPerCategory() {
+  knexInstance
+    .select('category')
+    .sum('price as total')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log('COST PER CATEGORY')
+      console.log(result)
+    })
+}
+
+costPerCategory()
